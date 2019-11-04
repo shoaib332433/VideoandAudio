aa747fbf6d76dff75d91d24b4f900b0255a7bdc1
shoaib332433 <shoaib.shah660@gmail.com>
2019-11-04 21:33:55 +0500

    Audio and Video is completed

--- app/src/main/java/com/example/videoandaudio/MainActivity.java
+++ app/src/main/java/com/example/videoandaudio/MainActivity.java
@@ -10,46 +10,63 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.MediaController;
 import android.widget.SeekBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener,SeekBar.OnSeekBarChangeListener,MediaPlayer.OnCompletionListener {
 
     private VideoView video;
     private Button startvideo,startmusic,pausemusic;
     private MediaController mediaController;
     private MediaPlayer mediaPlayer;
-    private SeekBar seekbar;
+    private SeekBar seekbar,musicseekbar;
     private AudioManager audioManager;
+    private Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // declare controls
         video=findViewById(R.id.video);
         startvideo=findViewById(R.id.startvideo);
         startmusic=findViewById(R.id.startmusic);
         pausemusic=findViewById(R.id.pausemusic);
         seekbar=findViewById(R.id.seekbar);
+        musicseekbar=findViewById(R.id.musicseekbar);
+
+
+        // Onclick methods
+        musicseekbar.setOnSeekBarChangeListener(MainActivity.this);
+        startvideo.setOnClickListener(MainActivity.this);
+        startmusic.setOnClickListener(MainActivity.this);
+        pausemusic.setOnClickListener(MainActivity.this);
+
+
 
         mediaController=new MediaController(MainActivity.this);
         mediaPlayer=MediaPlayer.create(this,R.raw.audion);
         audioManager= (AudioManager) getSystemService(AUDIO_SERVICE);
+        mediaPlayer.setOnCompletionListener(this);
 
+        // Max and normal sound
         int maxsound=audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
         int normalsound=audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
 
         seekbar.setMax(maxsound);
         seekbar.setProgress(normalsound);
-
-
-        startvideo.setOnClickListener(MainActivity.this);
-        startmusic.setOnClickListener(MainActivity.this);
-        pausemusic.setOnClickListener(MainActivity.this);
-
         seekbarchange();
+
+
+        musicseekbar.setMax(mediaPlayer.getDuration());
     }
 
+
+    // Onclick methods
 
     @Override
     public void onClick(View view) {
@@ -64,15 +81,24 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case R.id.startmusic:
                 mediaPlayer.start();
+                timer=new Timer();
+                timer.scheduleAtFixedRate(new TimerTask() {
+                    @Override
+                    public void run() {
+                        musicseekbar.setProgress(mediaPlayer.getCurrentPosition());
+                    }
+                },0,1000);
                 break;
 
             case R.id.pausemusic:
                 mediaPlayer.pause();
+                timer.cancel();
                 break;
         }
 
         }
 
+        // Sound change seekbar
         public void seekbarchange(){
             seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                 @Override
@@ -98,6 +124,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
+    // Music Progress Bar
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        if (fromUser){
+            mediaPlayer.seekTo(progress);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        mediaPlayer.pause();
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        mediaPlayer.start();
+    }
 
 
+
+    // MediaPlayer Oncompletion
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        timer.cancel();
+    }
 }
